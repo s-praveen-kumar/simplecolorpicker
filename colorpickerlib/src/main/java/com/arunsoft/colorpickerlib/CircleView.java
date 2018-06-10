72717e7e9068c37ef81ca5542962fb65fb357a84
S Praveen Kumar <35139208+s-praveen-kumar@users.noreply.github.com>
2018-06-10 19:59:02 +0530

    Initial commit

---
+++ colorpickerlib/src/main/java/com/arunsoft/colorpickerlib/CircleView.java
@@ -0,0 +1,50 @@
+package com.arunsoft.colorpickerlib;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+class CircleView extends View {
+
+    private int color = 0;
+    private Paint paint;
+
+    public CircleView(Context context) {
+        super(context);
+        paint = new Paint();
+    }
+
+    public CircleView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        paint = new Paint();
+    }
+
+    public CircleView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        paint = new Paint();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        paint.setColor(color);
+        canvas.drawCircle(getMeasuredWidth() / 2, getMeasuredHeight() / 2,getMeasuredWidth()/2, paint);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, widthMeasureSpec);
+    }
+
+    public int getColor() {
+        return color;
+    }
+
+    public void setColor(int color) {
+        this.color = color;
+        invalidate();
+    }
+}
