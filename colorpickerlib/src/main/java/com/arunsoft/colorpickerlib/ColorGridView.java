ae9da602f2b2affe219cc9cc5a58daaf1d575892
S Praveen Kumar <35139208+s-praveen-kumar@users.noreply.github.com>
2018-06-11 16:16:54 +0530

    Complete colorGridView with listener

--- colorpickerlib/src/main/java/com/arunsoft/colorpickerlib/ColorGridView.java
+++ colorpickerlib/src/main/java/com/arunsoft/colorpickerlib/ColorGridView.java
@@ -2,14 +2,16 @@ package com.arunsoft.colorpickerlib;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.GridView;
 
-public class ColorGridView extends GridView {
+public class ColorGridView extends GridView{
 
     private static final int SPACING = 8;
-    private int columns;
     private int[] colors;
     private ColorAdapter adapter;
+    private OnColorClickedListener listener;
 
     public ColorGridView(Context context) {
         super(context);
@@ -27,13 +29,20 @@ public class ColorGridView extends GridView {
     }
 
     private void init() {
-        columns = getResources().getInteger(R.integer.columns);
+        int columns = getResources().getInteger(R.integer.columns);
         colors = new int[]{0xffffffff};
         adapter = new ColorAdapter(getContext(), colors);
         setNumColumns(columns);
         setHorizontalSpacing(SPACING);
         setVerticalSpacing(SPACING);
         setAdapter(adapter);
+        setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if(listener!=null)
+                    listener.onColorClicked(colors[position]);
+            }
+        });
     }
 
     public void setColors(int[] colors, boolean supportTransparency) {
@@ -45,5 +54,13 @@ public class ColorGridView extends GridView {
         this.colors = colors;
         adapter.setColors(this.colors);
         adapter.notifyDataSetChanged();
+    }
+
+    public void setListener(OnColorClickedListener listener) {
+        this.listener = listener;
+    }
+
+    public interface OnColorClickedListener{
+        void onColorClicked(int color);
     }
 }
